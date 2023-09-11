b94127c76b56292e8dd39fc79b6b00fa7da25655
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-09-11 12:32:54 +0530

    Update AboutScreen.js

--- mobileapps/components/AboutScreen.js
+++ mobileapps/components/AboutScreen.js
@@ -1,1 +1,18 @@
 
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+
+const AboutScreen = () => {
+  const username = useSelector((state) => state.auth.username);
+
+  return (
+    <View>
+      <Text>About Page</Text>
+      {username && <Text>Hello, {username}!</Text>}
+    </View>
+  );
+};
+
+export default AboutScreen;
+
